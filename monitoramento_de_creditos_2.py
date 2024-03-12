21f636ee5f5dd3da55227f9995b2e322877c2b04
Kleber Rodrigues <kleberalmendro01@gmail.com>
2024-03-12 11:19:56 -0300

    ajuste

--- monitoramento_de_creditos_2.py
+++ monitoramento_de_creditos_2.py
@@ -57,7 +57,7 @@ new_entry2 = {
 new_entry3 = {
     'OWNER': 'MERCANTIL',
     'WAREHOUSE_NAME': 'COMPUTE_WH',
-    'TAG_NAME': 'ANÁLISE_CLIMÁTICA_SP',
+    'TAG_NAME': 'ANÁLISE_CRÉDITO',
     'END_TIME': '2024-02-09',
     'SOURCE': 'procces_1',
     'QUERY_TAG': 'result',
@@ -73,7 +73,7 @@ new_entry3 = {
 new_entry4 = {
     'OWNER': 'MERCANTIL',
     'WAREHOUSE_NAME': 'COMPUTE_WH',
-    'TAG_NAME': 'ANÁLISE_QUALIDADE_AR_SP',
+    'TAG_NAME': 'ANÁLISE_AR',
     'END_TIME': '2024-02-10',
     'SOURCE': 'procces_2',
     'QUERY_TAG': 'procces_1',
