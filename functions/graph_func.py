58c54978f9fc3dabf79d5112e5c7ac66feb5eb53
Kleber Rodrigues <kleberalmendro01@gmail.com>
2024-03-12 11:21:54 -0300

    revertando a função do grafo

--- functions/graph_func.py
+++ functions/graph_func.py
@@ -1,11 +1,4 @@
 from graphviz import Digraph
-from streamlit_agraph import agraph, Node, Edge, Config
-from streamlit_extras.switch_page_button import switch_page
-from st_pages import Page, show_pages, hide_pages
-from streamlit_modal import Modal
-import streamlit.components.v1 as components
-import streamlit as st
-
 
 def make_graph(df):
     """_summary_
@@ -41,67 +34,3 @@ def make_graph(df):
         dot.edge(row['SOURCE'], row['QUERY_TAG'])
 
     return dot
-
-def pop_up_graph():
-   pass
-    
-
-def make_agraph():
-    modal = Modal(
-        "Demo Modal", 
-        key="demo-modal",
-        
-        # Optional
-        padding=20,    # default value
-        max_width=744  # default value
-    )
-    open_modal = st.button("Open")
-    if open_modal:
-        modal.open()
-
-    if modal.is_open():
-        with modal.container():
-            st.write("Text goes here")
-
-            html_string = '''
-            <h1>HTML string in RED</h1>
-
-            <script language="javascript">
-            document.querySelector("h1").style.color = "red";
-            </script>
-            '''
-            components.html(html_string)
-    
-    nodes = []
-    edges = []
-    nodes.append( Node(id="Spiderman",
-                    title=st.button("Open", key=80),
-                    label="Peter Parker", 
-                    size=25, 
-                    shape="circularImage",
-                    image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_spiderman.png") 
-                ) # includes **kwargs
-    nodes.append( Node(id="Captain_Marvel", 
-                    size=25,
-                    shape="circularImage",
-                    image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_captainmarvel.png") 
-                )
-    edges.append( Edge(source="Captain_Marvel", 
-                    label="friend_of", 
-                    target="Spiderman", 
-                    # **kwargs
-                    ) 
-                ) 
-
-    config = Config(width=750,
-                    height=950,
-                    directed=True, 
-                    physics=True, 
-                    hierarchical=False,
-                    # **kwargs
-                    )
-
-    return_value = agraph(nodes=nodes, 
-                        edges=edges, 
-                        config=config)
-   
